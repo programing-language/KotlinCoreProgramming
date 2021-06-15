01b82a93a977ea98488926c9d705d381b1e8ce45
cjx <1067446576@qq.com>
2021-06-15 16:50:28 +0800

    lambda

---
+++ src/main/kotlin/basic/grammar/demo4Lambda/test3.kt
@@ -0,0 +1,17 @@
+package basic.grammar.demo4Lambda
+
+import basic.grammar.demo1.Country
+import basic.grammar.demo1.CountryApp
+import basic.grammar.demo1.CountryTest
+
+/**
+ * Created by chenjinxin on 2021/6/11 下午3:24
+ */
+fun main() {
+
+    val countryApp = CountryApp()
+    val countries = listOf<Country>()
+    countryApp.filterCountries(countries) { country ->
+        country.continient == "EU" && country.population > 10000
+    }
+}
