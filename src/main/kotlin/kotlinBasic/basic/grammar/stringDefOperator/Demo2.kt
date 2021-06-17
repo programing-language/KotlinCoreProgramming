9883d47f1a1289f7ded10601e7eae97211287f9e
cjx <1067446576@qq.com>
2021-06-17 17:29:02 +0800

    string

---
+++ src/main/kotlin/kotlinBasic/basic/grammar/stringDefOperator/Demo2.kt
@@ -0,0 +1,21 @@
+package kotlinBasic.basic.grammar.stringDefOperator
+
+/**
+ * Created by chenjinxin on 2021/6/17 下午5:19
+ */
+fun main() {
+    // 用这种3个引号定义的字符串，最终的打印格式与在代码中所呈现的格式一致，而不会解释转化转义字符（正如上述例子中的\n），以及Unicode的转义字符（如\uXXXX）。
+    val rawString = """
+    \n Kotlin is awesome.
+    \n Kotlin is a better Java."""
+    println(rawString)
+
+    val html = """
+    <html>
+       <body>
+           <p>Hello World.</p>
+       </body>
+    </html>
+           """
+    println(html)
+}
