7003382c96689857475884e2bdff5af35db25678
cjx <1067446576@qq.com>
2021-06-25 15:39:33 +0800

    componentN

--- src/main/kotlin/core/obj/oriented/dataClass/componentNDemo/Bird.kt
+++ src/main/kotlin/core/obj/oriented/dataClass/componentNDemo/Bird.kt
@@ -3,6 +3,9 @@ package core.obj.oriented.dataClass.componentNDemo
 /**
  * Created by chenjinxin on 2021/6/25 下午3:32
  */
+/**
+ * 数据类中的解构基于componentN函数，如果自己不声明componentN函数，那么就会默认根据主构造函数参数来生成具体个数的componentN函数，与从构造函数中的参数无关。
+ */
 data class Bird(var weight: Double, var age: Int, var color: String) {
     var sex = 1
     operator fun component4(): Int {//operator关键字
