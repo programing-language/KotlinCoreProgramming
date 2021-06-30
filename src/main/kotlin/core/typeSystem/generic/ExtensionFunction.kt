20fa1a38a998ffb7b2b999572cd21084507168b5
cjx <1067446576@qq.com>
2021-06-30 11:13:29 +0800

    泛型，扩展函数

--- src/main/kotlin/core/typeSystem/generic/ExtensionFunction.kt
+++ src/main/kotlin/core/typeSystem/generic/ExtensionFunction.kt
@@ -3,5 +3,14 @@ package core.typeSystem.generic
 /**
  * Created by ChenJinXin on 2021/6/30 上午11:01
  */
-class ExtensionFunction {
+fun <T> ArrayList<T>.find(t: T): T? {
+    val index = this.indexOf(t)
+    return if (index >= 0) this[index] else null
+}
+
+fun main() {
+    val arrayList = ArrayList<String>()
+    arrayList.add("one")
+    println(arrayList.find("one"))//输出 one
+    println(arrayList.find("two").isNullOrEmpty())//输出true
 }
