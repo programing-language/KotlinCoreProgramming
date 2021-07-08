12107a9daed1aaa3369e2f4fcd3b1fd7429b774b
cjx <1067446576@qq.com>
2021-07-08 17:44:38 +0800

    vetoable提供了一种功能，在被赋值之前拦截，然后判断是否接受它

--- src/main/kotlin/exploration/designPatterns/behavioral/observerMode/VetoableDemo.kt
+++ src/main/kotlin/exploration/designPatterns/behavioral/observerMode/VetoableDemo.kt
@@ -1,5 +1,17 @@
 package exploration.designPatterns.behavioral.observerMode
 
+import kotlin.properties.Delegates
+
 /**
  * Created by ChenJinXin on 2021/7/8 下午5:41
  */
+var value: Int by Delegates.vetoable(0) { prop, old, new ->
+    new > 0
+}
+
+fun main() {
+    value = 1
+    println(value)
+    value = -1
+    println(value)
+}
