3442106a1dd0f5942f47077a3da7f89f4e75a14d
cjx <1067446576@qq.com>
2021-07-08 18:38:50 +0800

    通过重载iterator方法优化迭代器模式

--- src/main/kotlin/exploration/designPatterns/behavioral/iteratorMode/Upgrade.kt
+++ src/main/kotlin/exploration/designPatterns/behavioral/iteratorMode/Upgrade.kt
@@ -3,21 +3,16 @@ package exploration.designPatterns.behavioral.iteratorMode
 /**
  * Created by ChenJinXin on 2021/7/8 下午6:30
  */
-data class Book2(val name: String)
-class Bookcase2(private val books: List<Book2>) {
-    operator fun iterator(): Iterator<Book2> = this.books.iterator()
+class Bookcase2(val books: List<Book>) {
+    operator fun iterator(): Iterator<Book> = this.books.iterator()
 }
 
 fun main() {
-    val bookcase = Bookcase(
+    val bookcase = Bookcase2(
             listOf(Book("Dive into Kotlin"), Book("Thinking in Java"))
     )
 
-    while (bookcase.hasNext()) {
-        println("The book name is ${bookcase.next().name}")
-    }
-
-    /*for (boot in bookcase) {
+    for (boot in bookcase) {
         println("The book name is ${boot.name}")
-    }*/
+    }
 }
