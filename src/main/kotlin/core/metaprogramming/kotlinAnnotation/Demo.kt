643903528e6367a0ff03a0e49435576fd187b24c
cjx <1067446576@qq.com>
2021-07-08 11:41:37 +0800

    Kotlin注解

---
+++ src/main/kotlin/core/metaprogramming/kotlinAnnotation/Demo.kt
@@ -0,0 +1,26 @@
+package core.metaprogramming.kotlinAnnotation
+
+import kotlin.reflect.full.declaredMemberProperties
+
+/**
+ * Created by ChenJinXin on 2021/7/8 上午11:23
+ */
+annotation class Cache(val namespace: String, val expires: Int)
+annotation class CacheKey(val keyName: String, val buckets: IntArray)
+
+@Cache(namespace = "hero", expires = 3600)
+data class Hero(
+        @property:CacheKey(keyName = "heroName", buckets = intArrayOf(1, 2, 3))
+        val name: String,
+        @field:CacheKey(keyName = "heroName", buckets = intArrayOf(1, 2, 3))
+        val attack: Int,
+        @get:CacheKey(keyName = "heroName", buckets = intArrayOf(1, 2, 3))
+        val defense: Int,
+        val initHp: Int
+)
+
+fun main() {
+        val cacheAnnotion = Hero::class.annotations.find { it is Cache }as Cache?
+        println("namespace ${cacheAnnotion?.namespace}")
+        println("expires ${cacheAnnotion?.expires}")
+}
