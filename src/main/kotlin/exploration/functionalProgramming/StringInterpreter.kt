b2c3aa4fb74eb4fb3555d3b54e8d8cdc96031fd4
cjx <1067446576@qq.com>
2021-07-09 18:15:49 +0800

    纯函数与引用透明性

---
+++ src/main/kotlin/exploration/functionalProgramming/StringInterpreter.kt
@@ -0,0 +1,18 @@
+package exploration.functionalProgramming
+
+/**
+ * Created by ChenJinXin on 2021/7/9 下午5:56
+ */
+fun stringInterpreter(str: List<Format>) = str.fold("") { fullText, s ->
+    when (s) {
+        is Print -> fullText + s.text
+        is Newline -> fullText + "\n"
+    }
+}
+
+fun main() {
+    val format1 = Print("format1")
+    val format2 = Print("format2")
+    val result = stringInterpreter(listOf(format1,Newline,format2))
+    println(result)
+}
---
+++ src/main/kotlin/exploration/functionalProgramming/UnsafeInterpreter.kt
@@ -0,0 +1,25 @@
+package exploration.functionalProgramming
+
+/**
+ * Created by ChenJinXin on 2021/7/9 下午5:44
+ */
+sealed class Format
+data class Print(val text: String) : Format()
+object Newline : Format()
+
+val string = listOf<Format>(Print("Hello"), Newline, Print("Kotlin"))
+
+fun unsafeInterpreter(str: List<Format>) {
+    str.forEach {
+        when (it) {
+            is Print -> print(it.text)
+            is Newline -> println()
+        }
+    }
+}
+
+fun main() {
+    val format1 = Print("format1")
+    val format2 = Print("format2")
+    unsafeInterpreter(listOf(format1,Newline,format2))
+}
